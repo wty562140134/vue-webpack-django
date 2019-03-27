517ed37296258c84445ec83848f7b2a812890fef
Your Name <you@example.com>
2019-03-27 11:56:57 +0800

    demo更新

--- crmpages/src/modules/login/App.vue
+++ crmpages/src/modules/login/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <!--<button @click="testRefs" ref="button1">点击一下</button>-->
+    <!--<input ref="input1" text="123"></input>-->
     <router-view/>
   </div>
 </template>
@@ -15,7 +17,32 @@
         msg: '初始页面msg',
       }
     },
-    methods: {},
+    methods: {
+      /*
+      测试vue操作dom
+      */
+      testRefs() {
+        this.$refs.input1.placeholder = "????"
+        this.$refs.button1.innerHTML = "我要改变世界改变自己"
+      }
+    },
+
+    beforeCreate() {
+      /*
+      虽然vue不提倡操纵dom元素,
+      但是由于body是在页面还未渲染之前就有的,
+      此时vue内的el对象还没生产,所以无法通过el来操作dom,
+      所以只能通过这种方式来给body设置图片
+       */
+      document.querySelector('body').setAttribute('style', 'background-image:url(/static/2b.jpg);background-repeat:no-repeat;width;100%;height: 100%;')
+    },
+
+    mounted() {
+      console.log(this)
+      this.$refs.input1.placeholder = "????"
+      this.$refs.button1.innerHTML = "我要改变世界改变自己"
+    }
+
   }
 </script>
 
