22efb812763578c4fb15721ff01a68e972af1567
Your Name <you@example.com>
2019-04-02 09:18:07 +0800

    新增公共路由组件和路由拦截器

--- crmpages/src/modules/login/App.vue
+++ crmpages/src/modules/login/App.vue
@@ -1,38 +1,20 @@
 <template>
   <div id="app">
-    <button @click="testRefs" ref="button1">点击一下</button>
-    <input ref="input1" text="123"></input>
     <router-view/>
   </div>
 </template>
 
 <script>
-  // 如果需要使用<login></login>标签来进行渲染则需要通过引入login组件,并在components中定义,若是通过<router-view/>标签来通过路由匹配渲染则不需要
-  // import login from "./components/login"
 
   export default {
     name: 'app',
-    // components: {login},
     data() {
       return {
         msg: '初始页面msg',
       }
     },
-    methods: {
-      /*
-      测试vue操作dom
-      */
-      testRefs() {
-        this.$refs.input1.placeholder = "????"
-        this.$refs.button1.innerHTML = "我要改变世界改变自己"
-      }
-    },
 
-    // mounted() {
-    //   console.log(this)
-    //   this.$refs.input1.placeholder = "????"
-    //   this.$refs.button1.innerHTML = "我要改变世界改变自己"
-    // }
+    methods: {},
 
   }
 </script>
---
+++ crmpages/src/modules/vueTest/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ref_test/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import ref_test from "./components/reftest";
+
+  export default {
+    name: "App",
+    components: {ref_test}
+  }
+</script>
+
+<style scoped>
+
+</style>
