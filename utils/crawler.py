cbc5654e9fa7a4659bdeab47557c691738e2529e
Your Name <you@example.com>
2019-04-29 13:53:17 +0800

    111

--- utils/crawler.py
+++ utils/crawler.py
@@ -1,25 +1,45 @@
-from urllib.error import URLError, HTTPError
+from urllib.error import HTTPError
 import urllib.parse as parse
 import urllib.request as request
 import hashlib
 import http.cookiejar
 import simplejson
-import zlib
-import chardet
 import os
+from .config_util import yaml_util
+
+# 获取本模块路径
+current_path = os.path.abspath(os.path.dirname(__file__))
+# 获取本模块下配置文件中的数据对象
+configs = yaml_util.YamlLoadUtil(current_path + '/crawle_config.yaml').get_config_data()
 
 
 class Crawler:
+    """
+    爬取数据的类
+    """
+
     def __init__(self, url, header):
+        """
+        爬取数据类构造函数
+        :param url: 需要爬取的数据的url
+        :param header: 爬取数据的header信息
+        """
         self._url = url
         self._headers = header
 
     def open_url(self, data, save_cookie_file_name, salt=None):
-        if (salt is not None):
+        """
+        发送请求的函数
+        :param data: 发送请求的参数
+        :param save_cookie_file_name: 需要将cookie保存到文件的文件名
+        :param salt: 如果是登录,则需要将密码加密,密码加密所用的盐
+        :return: http响应对象
+        """
+        if salt is not None:
             # 把密码加密
-            data['password'] = self.md5_passwd(data['password'], salt)
+            data['password'] = self.md5_password(data['password'], salt)
         # 这里判断有没有data是为了区分是post还是get请求
-        if (data is None):
+        if data is None:
             # 创建get请求对象
             req = request.Request(self._url, headers=self._headers)
         else:
@@ -29,13 +49,25 @@ class Crawler:
         response = self.cookies_options(req, save_cookie_file_name)
         return response
 
-    def md5_passwd(self, data, salt):
+    def md5_password(self, password, salt):
+        """
+        使用md5将密码加密函数
+        :param password: 登录的密码
+        :param salt: 将密码加密用的盐
+        :return: 加密后的密码
+        """
         md = hashlib.md5()
-        data = data + salt
-        md.update(data.encode('utf-8'))
+        password_md5 = password + salt
+        md.update(password_md5.encode('utf-8'))
         return md.hexdigest()
 
     def cookies_options(self, req, file_name):
+        """
+        操作cookie的函数
+        :param req: http请求对象
+        :param file_name: 保存cookie的文件名称
+        :return: http响应对象
+        """
         # 创建cookie对象
         cookie_data = http.cookiejar.MozillaCookieJar(file_name)
         # 创建cookie处理器
@@ -44,7 +76,7 @@ class Crawler:
         opener = request.build_opener(handler)
         try:
             # 判断是否有存放cookie的文件
-            if (not os.path.exists(file_name)):
+            if not os.path.exists(file_name):
                 # 进来这里说明没有这个文件,先发送请求,然后储存cookie到cookie文件中
                 response = opener.open(req)
                 cookie_data.save(ignore_discard=True, ignore_expires=True)
@@ -56,126 +88,255 @@ class Crawler:
             return response
         except HTTPError as e:
             # 请求url如果没错的话请求404一般是应为cookie没有导致的
-            if (e.code == 404):
+            if e.code == 404:
                 # 判断是否有储存cookie的文件,有则将文件删除
-                if (os.path.exists(file_name)):
+                if os.path.exists(file_name):
                     os.remove(file_name)
-                # 从新模拟登陆获取新的cookie
-            login()
-        except URLError as e:
-            print(e.reason)
+                    # 从新模拟登陆获取新的cookie
+                    login()
+            e.msg
 
 
-def action(url, headers, data=None, file_name='./cookie.txt', salt=None):
+def action(url, headers, data=None, file_name='cookie.txt', salt=None):
+    """
+    执行整个爬取数据的函数
+    :param url: 需要爬取数据的url
+    :param headers: 爬取数据的http的header
+    :param data: 请求的参数
+    :param file_name: cookie保存的文件,默认是cookie.txt
+    :param salt: 将密码加密所用的盐
+    :return: http响应对象
+    """
+    # 获取爬取数据对象
     c = Crawler(url, headers)
-    if (salt is not None):
+    # 判断是否有加密盐,如果有则是登录,没有则是数据爬取
+    if salt is not None:
         return c.open_url(data=data, save_cookie_file_name=file_name, salt=salt)
     else:
         return c.open_url(data=data, save_cookie_file_name=file_name)
 
 
 def headers():
+    """
+    :return: http请求头
+    """
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Content - Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'www.gsafetycloud.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
         # 'Connection': 'keep - alive',
         # 'Referer': 'http//www.gsafetycloud.com/operation-management/index.html',
-        'Content - Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         # 'X - Requested - With': 'XMLHttpRequest',
-        'Host': 'www.gsafetycloud.com',
         # 'Origin': 'http://www.gsafetycloud.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
         # 'cookie':cookie
     }
     return headers
 
 
 def format_data(data, char_set='utf-8'):
+    """
+    将响应对象数据格式化为json对象函数
+    :param data: 响应对象
+    :param char_set: 格式化的编码,默认为utf-8
+    :return: 格式化为json的json对象
+    """
     try:
-        data = simplejson.loads(data.read().decode(char_set))
+        return_data = simplejson.loads(data.read().decode(char_set))
+        if 'data' in return_data:
+            return return_data['data']
+        return return_data
     except AttributeError as e:
-        print(e)
+        if e.__str__() == "'NoneType' object has no attribute 'read'":
+            login()
+        else:
+            raise e
+
+
+def fh_send_get(interface, params, api_url):
+    """
+    爬取消防管家get请求数据函数
+    :param interface: 爬取数据接口
+    :param params: 发送请求参数
+    :param api_url: 发送请求的api_url
+    :return: 爬取到的数据
+    """
+    interface_url = format_url(api_url)
+    data = crawl_data(interface_url.format(interface, params))
     return data
 
 
+def fh_send_post(interface, params, api_url):
+    """
+    爬取消防管家post请求数据函数
+    :param interface: 爬取数据接口
+    :param params: 发送请求参数
+    :param api_url: 发送请求的api_url 默认是配置文件中的base_url
+    :return: 爬取到的数据
+    """
+    interface_url = format_url(api_url).format(interface)
+    data = crawl_data(url=interface_url, data=params)
+    return data
+
+
+# 若未登陆外部使用login作为登陆模拟的函数
 def login():
-    data = {
-        'loginName': 'km01',
-        'password': 'a123456',
-        'systemCode': ''
-    }
-    login_url = 'http://www.gsafetycloud.com/operation-management/login/toLogin1.mvc'
-    # 模拟登陆储存cookie
-    salt = 'true'
+    """
+    模拟登录的函数
+    :return: 登录是否成功响应
+    """
+    data = configs['login_data']
+    login_url = format_url(configs['api_url']['toLogin'])
+    # 密码加密盐
+    salt = configs['salt']
     return format_data(action(login_url, headers(), data=data, salt=salt))
 
 
+# 外部使用主要函数
 def crawl_data(url, data=None):
+    """
+    外部使用爬取数据主要函数
+    :param url: 需要爬取数据的url
+    :type url:str
+    :param data: 发送请求的参数
+    :type data:dict or str
+    :return: http请求响应数据
+    """
     return format_data(action(url=url, headers=headers(), data=data))
 
 
-if __name__ == '__main__':
-    forent_url = {
-        'backUrl': 'http://www.gsafetycloud.com/operation-management',
-        'operation_url': 'http://www.gsafetycloud.com/api/v1.1/operation-management',
-        'base_url': 'http://www.gsafetycloud.com/api/v1/fire-society',
-    }
+# 外部使用主要函数
+def format_url(api_url):
+    """
+    外部使用格式化url主要函数函数,将系统域名与api_url进行拼接
+    :param api_url: 发送请求的url
+    :type api_url:str
+    :return: 格式化好的url
+    """
+    return configs['system_url'].format(api_url)
 
-    # # 获取用户信息,如果出错表面未登陆
-    # user_msg = crawl_data(forent_url['backUrl'] + '/user/getUserMsg.mvc', data={})
-    # print(user_msg)
-    #
-    # # 获取地图所有数据
-    # map_sarch_data = {
-    #     'orgCode': user_msg['orgCode'],
-    #     "unitState": "",
-    #     "queryType": "012"
-    # }
-    # map_data = crawl_data(forent_url['operation_url'] + '/gisView/mapLocation', map_sarch_data)
-    #
-    # # 遍历地图数据
-    # for i in map_data['data']:
-    #     if ('unitType' in i):
-    #         print(i)
-    #         print('单位编码:[', i['id'], ']', '单位名称:[', i['name'], ']', '单位类型:[', i['unitType'], ']', '单位状态:[',
-    #               i['unitState'], ']', '单位地址:[', i['address'], ']', '单位经纬度:[', i['latitude'], '&',
-    #               i['longitude'], ']')
 
-    # 获取消防管家地图数据和消防管家实时报警,检测故障,当前隐患数接口
-    fire_housekeeper_urls_get = [
-        'getChildOrg',  # 地图数据
-        'getInfoNum',  # 实时报警,检测故障,当前隐患数
-    ]
+# 外部使用主要函数
+def get_fh_data(interface_list, params, api_url, appoint_interface=None):
+    """
+    外部使用爬取消防管家数据的主要函数
+    :param interface_list: 需要爬取数据的接口list
+    :type interface_list:list
+    :param params: 发送请求的参数
+    :type params:dict
+    :param api_url: 发送请求的url
+    :type api_url:str
+    :param appoint_interface: 向指定接口发送请求,默认为None,如果为None则取所有接口数据
+    :type appoint_interface:str
+    :return: 指定爬取接口数据或者所有接口数据
+    """
+    all_data = []
+    for interface in interface_list:
+        # 判断是否有指定接口名,没有则取所有
+        if appoint_interface is None:
+            # 判断请求参数的类型是str则发送get请求
+            if isinstance(params, str):
+                data = fh_send_get(interface, params, api_url)
+            # 是dict则发送post请求
+            elif isinstance(params, dict):
+                data = fh_send_post(interface, params, api_url)
+            all_data.append(data)
+        else:
+            # 有指定接口名则只取指定接口数据
+            if appoint_interface == interface:
+                # 判断请求参数的类型是str则发送get请求
+                if isinstance(params, str):
+                    return fh_send_get(appoint_interface, params, api_url)
+                # 是dict则发送post请求
+                elif isinstance(params, dict):
+                    return fh_send_post(appoint_interface, params, api_url)
+            # 不是指定的接口则跳过该次循环
+            else:
+                continue
+    return all_data
 
-    # 获取消防管家地图 实时报警 监测故障 当前隐患信息接口
-    fire_housekeeper_urls_post = [
-        'queryTodayAlarmCountTop5',  # 实时报警
-        'queryTodayAccidentCheckCountTop5',  # 监测故障
-        'queryTodayHiddenCountTop5',  # 当前隐患
-        'queryAlarmInfo',  # 企业报警信息
-        'queryThirtyAlarmCountInfo',  # 报警趋势
-        'queryThirtyAccidentCountInfo',  # 隐患趋势
-    ]
-    # 消防管家加载用户信息
-    req_user_info_data = {
-        'loginName': 'whxfdd'
-    }
-    user_info = crawl_data(forent_url['base_url'] + '/sys/user/getUserMsg.mvc', data=req_user_info_data)
-    print('消防管家加载用户信息:', user_info)
 
-    # 消防管家http头
-    http_head_data = crawl_data(forent_url['base_url'] + '/sys/org/' + user_info['orgCode'])
-    print('消防管家http头:', http_head_data)
+# 外部使用主要函数
+def format_get_params(req_params):
+    """
+    用于格式化GET请求的函数,将dict格式化为字符串: key=value的形式
+    :param req_params: GET请求的参数dict类型
+    :type req_params:dict
+    :return: GET请求的字符串参数
+    """
+    # 初始化一个GET请求参数的空字符串
+    get_params = ''
+    # 建立一个索引用于判断是否和字典长度相等
+    index = 1
+    # 遍历字典
+    for k, v in req_params.items():
+        # 建立一个GET请求的key=value形式的字符串key和value用placeholder代替
+        get_str = '{}={}'
+        # 判断index是否等于字典len,如果等于则不需要再末尾添加&,否则需要添加&
+        if index == len(req_params):
+            # 将key和value分别替换占位符
+            get_str = get_str.format(k, v)
+        else:
+            # 将key和value分别替换占位符
+            get_str = get_str.format(k, v)
+            # 在末尾添加&符号
+            get_str += '&'
+        # 把拼接好的字符串拼接到GET请求参数的字符串中
+        get_params += get_str
+        # 索引+1
+        index += 1
+    # 返回GET请求参数的字符串
+    return get_params
 
-    # 获取消防管家地图数据和消防管家实时报警,检测故障,当前隐患数
-    for i in fire_housekeeper_urls_get:
-        url = (forent_url['base_url'] + '/group/index/{}?orgCode=' + user_info['orgCode']).format(i)
-        data = crawl_data(url)
-        print(data)
 
-    # 获取消防管家地图 实时报警 监测故障 当前隐患信息
-    for i in fire_housekeeper_urls_post:
-        url = (forent_url['base_url'] + '/group/index/{}').format(i)
-        print(url)
-        data = crawl_data(url=url, data={'orgCode': user_info['orgCode']})
-        print(data)
+# 外部使用主要函数
+def get_img(img_url, save_path=current_path + '/' + 'floor_img'):
+    """
+    爬取图片函数
+    :param img_url:爬取图片的url
+    :type img_url:str
+    :param save_path: 爬取到的图片保存的位置,默认为爬虫工具类所在的路径下的/floor_img
+    :type save_path:str
+    :return: 无返回值
+    """
+    if os.path.exists(save_path):
+        pass
+    else:
+        os.makedirs(save_path)
+    img = img_url.split('/')
+    img_name = save_path + '\\' + img[-3] + '_' + img[-2] + '_' + img[-1]
+    try:
+        request.urlretrieve(img_url, img_name)
+    except HTTPError as e:
+        if e.code == 404:
+            print(img_url, '404!!!!!!!!!!')
+        e.msg
 
+# if __name__ == '__main__':
+#     """
+#     使用demo
+#     """
+# login_data = login()
+# print(login_data)
+# # 获取用户信息,如果出错表面未登陆
+# get_user_msg_url = format_url(configs['api_url']['getUserMsg'])
+# user_msg = crawl_data(get_user_msg_url, data={})
+# print(user_msg)
+#
+# # 获取地图所有数据
+# map_serch_data = configs['map_serch_data']
+# # 替换配置文件中的占位符
+# map_serch_data['orgCode'] = map_serch_data['orgCode'].format(user_msg['orgCode'])
+# print(map_serch_data)
+# map_data = get_fh_data(configs['post_interface_url'], map_serch_data,
+#                        configs['api_url']['mapLocation'],
+#                        appoint_interface='mapLocation')
+# print('map_data', map_data)
+#
+# # 遍历地图数据
+# for i in map_data:
+#     if 'unitType' in i:
+#         print(i)
+#         print('单位编码:[', i['id'], ']', '单位名称:[', i['name'], ']', '单位类型:[', i['unitType'], ']', '单位状态:[',
+#               i['unitState'], ']', '单位地址:[', i['address'], ']', '单位经纬度:[', i['latitude'], '&',
+#               i['longitude'], ']')
---
+++ utils/crawler_task.py
@@ -0,0 +1,130 @@
+import schedule
+import threading
+import time
+# from whbigdata.crawler.crawle_utils.db_util import DBConnect
+# from whbigdata.crawler.crawle_utils.crawler import get_fh_data, configs, format_get_params
+# from crawler.crawle_utils.db_util import DBConnect
+# from crawler.crawle_utils.crawler import get_fh_data, configs, format_get_params
+from crawle_utils.db_util import DBConnect
+from crawle_utils.crawler import get_fh_data, configs, format_get_params
+
+
+class Task:
+    def __init__(self, jobs_fun):
+        self._jobs = list(jobs_fun)
+
+    def _thread_task(self):
+        for job in self._jobs:
+            threading.Thread(target=job).start()
+
+    def run(self, task_rule_instance, sleep_time=0):
+        task_rule_instance.do(self._thread_task)
+        while True:
+            schedule.run_pending()
+            time.sleep(sleep_time)
+
+
+def get_org_task():
+    req_params = {'orgCode': 'WHXFDD_JT_201910110'}
+    map_data = get_fh_data(configs['get_interface_url'], format_get_params(req_params),
+                           configs['api_url']['getChildOrg'],
+                           appoint_interface='getChildOrg')
+    connect = DBConnect(configs['data_base'])
+    with connect as db:
+        is_not_new_data(db, map_data)
+
+
+def is_not_new_data(db, map_data, select_sql='select * from t_units where fs_unit_sn=%(orgCode)s'):
+    for i in map_data:
+        unit = db.query(select_sql, {'orgCode': i['orgCode']})
+        if unit is None:
+            insert(db, i)
+        else:
+            update(db, unit, i)
+
+
+def insert(db, insert_data, insert_sql='insert into t_units(fs_unit_sn, fs_unit_name, '
+                                       'fd_unit_lat, fd_unit_lng, fi_unit_status, fs_unit_addr, fi_unit_type) '
+                                       'value (%(orgCode)s, %(orgName)s, %(lat)s, %(log)s, %(state)s, " ", 0)'):
+    db.commit(insert_sql, insert_data)
+
+
+def update(db, unit, map_data):
+    update_data_list = []
+    update_sql = 'update t_units set '
+    where = ' where fi_unit_id=%(id)s'
+    update_data = {}
+    if unit['fs_unit_sn'] == map_data['orgCode']:
+
+        if unit['fs_unit_name'] != map_data['orgName']:
+            update_sql += 'fs_unit_name=%(orgName)s'
+            update_data = {'orgName': map_data['orgName'], 'id': unit['fi_unit_id']}
+            update_data_list.append(update_data)
+
+        map_data['lat'] = float(map_data['lat'])
+        if unit['fd_unit_lat'] != map_data['lat']:
+            if update_data.__len__() != 0:
+                update_sql += ', fd_unit_lat=%(lat)s'
+            else:
+                update_sql += 'fd_unit_lat=%(lat)s'
+            update_data = {'lat': map_data['lat'], 'id': unit['fi_unit_id']}
+            update_data_list.append(update_data)
+
+        map_data['log'] = float(map_data['log'])
+        if unit['fd_unit_lng'] != map_data['log']:
+            if update_data.__len__() != 0:
+                update_sql += ', fd_unit_lng=%(log)s'
+            else:
+                update_sql += 'fd_unit_lng=%(log)s'
+            update_data = {'log': map_data['log'], 'id': unit['fi_unit_id']}
+            update_data_list.append(update_data)
+
+        map_data['state'] = int(map_data['state'])
+        if unit['fi_unit_status'] != map_data['state']:
+            if update_data.__len__() != 0:
+                update_sql += ', fi_unit_status=%(state)s'
+            else:
+                update_sql += 'fi_unit_status=%(state)s'
+            update_data = {'state': map_data['state'], 'id': unit['fi_unit_id']}
+            update_data_list.append(update_data)
+
+    if update_data_list.__len__() > 0:
+        update_sql = update_sql + where
+        db.commit(update_sql, update_data_list)
+
+
+def run_task(*job, every=None, when_time=None, at_day_start_time='', to=None):
+    """
+    使用示例:
+    schedule.every(10).minutes 每隔十分钟执行一次任务
+    schedule.every().hour 每隔一小时执行一次任务
+    schedule.every().day.at("10:30") 每天的10:30执行一次任务
+    schedule.every(5).to(10).days 每隔5到10天执行一次任务
+    schedule.every().monday 每周一的这个时候执行一次任务
+    schedule.every().wednesday.at("13:15") 每周三13:15执行一次任务
+
+    :param job: 需要运行的任务函数,是一个函数对象
+    :type object
+    :param every: schedule.every()中的传参
+    :type int
+    :param when_time: 是schedule中的second,minute,hours，day等
+    :type str
+    :param at_day_start_time:当schedule使用day时，例如schedule.every().day.at() at()中的传参
+    :type str
+    :param to: 当schedule需要在某一时间段或每过几天之内执行,例如schedule.every(5).to(10).days
+    :type int
+    """
+    if when_time == 'day':
+        if every is not None:
+            sch = eval('schedule.every({}).{}.at("{}")'.format(every, when_time, at_day_start_time))
+        else:
+            sch = eval('schedule.every().{}.at("{}")'.format(when_time, at_day_start_time))
+        if to is not None:
+            sch = eval('schedule.every({}).to({}).{}'.format(every, to, when_time))
+    else:
+        sch = eval('schedule.every({}).{}'.format(every, when_time))
+    t = Task(job)
+    t.run(sch)
+
+
+run_task(get_org_task, when_time=configs['task']['when_time'], at_day_start_time=configs['task']['at_day_start_time'])
