4b782cb314d5ef81a2d648d328be923b83f5f1bc
Your Name <you@example.com>
2019-05-05 15:04:12 +0800

    111

--- crawle_utils/crawler_task.py
+++ crawle_utils/crawler_task.py
@@ -1,8 +1,8 @@
 import schedule
 import threading
-from whbigdata.crawler.crawle_utils.db_util import DBConnect
-from whbigdata.crawler.crawle_utils.crawler import get_fh_data, configs, format_get_params
-from whbigdata.crawler.crawle_utils.zb import pmtobd
+from .db_util import DBConnect
+from .crawler import get_fh_data, configs, format_get_params
+from .zb import pmtobd
 
 
 # from crawler.crawle_utils.db_util import DBConnect
@@ -38,15 +38,6 @@ class Task:
                 t.join()
 
 
-def is_not_new_data(db, web_data, select_sql='select * from t_units where fs_unit_sn=%(orgCode)s'):
-    for i in web_data:
-        unit = db.query(select_sql, {'orgCode': i['orgCode']})
-        if unit is None:
-            insert(db, i)
-        else:
-            update(db, unit, i)
-
-
 def insert(db, insert_data, insert_sql='insert into t_units(fs_unit_sn, fs_unit_name, '
                                        'fd_unit_lat, fd_unit_lng, fi_unit_status, fs_unit_addr, fi_unit_type) '
                                        'value (%(orgCode)s, %(orgName)s, %(lat)s, %(log)s, %(state)s, " ", 0)'):
@@ -58,45 +49,6 @@ def update(db, data_base_data, web_data, update_sql='update t_units set ', where
     if update_data_list.__len__() > 0:
         update_sql = update_sql + where
         db.commit(update_sql, update_data_list)
-
-
-def set_update_data(data_base_data, web_data, update_sql):
-    update_data_list = []
-    update_data = {}
-    if data_base_data['fs_unit_sn'] == web_data['orgCode']:
-
-        if data_base_data['fs_unit_name'] != web_data['orgName']:
-            update_sql += 'fs_unit_name=%(orgName)s'
-            update_data['orgName'] = web_data['orgName']
-
-        web_data['lat'] = float(web_data['lat'])
-        if data_base_data['fd_unit_lat'] != web_data['lat']:
-            if update_data.__len__() != 0:
-                update_sql += ', fd_unit_lat=%(lat)s'
-            else:
-                update_sql += 'fd_unit_lat=%(lat)s'
-            update_data['lat'] = web_data['lat']
-
-        web_data['log'] = float(web_data['log'])
-        if data_base_data['fd_unit_lng'] != web_data['log']:
-            if update_data.__len__() != 0:
-                update_sql += ', fd_unit_lng=%(log)s'
-            else:
-                update_sql += 'fd_unit_lng=%(log)s'
-            update_data['log'] = web_data['log']
-
-        web_data['state'] = int(web_data['state'])
-        if data_base_data['fi_unit_status'] != web_data['state']:
-            if update_data.__len__() != 0:
-                update_sql += ', fi_unit_status=%(state)s'
-            else:
-                update_sql += 'fi_unit_status=%(state)s'
-            update_data['state'] = web_data['state']
-
-        if update_data.__len__() > 0:
-            update_data['id'] = data_base_data['fi_unit_id']
-            update_data_list.append(update_data)
-    return update_sql, update_data_list, update_data
 
 
 def set_task(job, every=None, when_time=None, at_day_start_time='', to=None):
@@ -140,7 +92,6 @@ def get_org_task():
                            configs['api_url']['getChildOrg'],
                            appoint_interface='getChildOrg')
     connect = DBConnect(configs['data_base'])
-    print(map_data)
     for i in map_data:
         i['log'] = float(i['log'])
         i['lat'] = float(i['lat'])
@@ -148,6 +99,54 @@ def get_org_task():
         i['lat'], i['log'] = pmtobd(i['lat'], i['log'])
     with connect as db:
         is_not_new_data(db, map_data)
+
+
+def is_not_new_data(db, web_data, select_sql='select * from t_units where fs_unit_sn=%(orgCode)s'):
+    for i in web_data:
+        unit = db.query(select_sql, {'orgCode': i['orgCode']})
+        if unit is None:
+            insert(db, i)
+        else:
+            update(db, unit, i)
+
+
+def set_update_data(data_base_data, web_data, update_sql):
+    update_data_list = []
+    update_data = {}
+    if data_base_data['fs_unit_sn'] == web_data['orgCode']:
+
+        if data_base_data['fs_unit_name'] != web_data['orgName']:
+            update_sql += 'fs_unit_name=%(orgName)s'
+            update_data['orgName'] = web_data['orgName']
+
+        web_data['lat'] = float(web_data['lat'])
+        if data_base_data['fd_unit_lat'] != web_data['lat']:
+            if update_data.__len__() != 0:
+                update_sql += ', fd_unit_lat=%(lat)s'
+            else:
+                update_sql += 'fd_unit_lat=%(lat)s'
+            update_data['lat'] = web_data['lat']
+
+        web_data['log'] = float(web_data['log'])
+        if data_base_data['fd_unit_lng'] != web_data['log']:
+            if update_data.__len__() != 0:
+                update_sql += ', fd_unit_lng=%(log)s'
+            else:
+                update_sql += 'fd_unit_lng=%(log)s'
+            update_data['log'] = web_data['log']
+
+        web_data['state'] = int(web_data['state'])
+        if data_base_data['fi_unit_status'] != web_data['state']:
+            if update_data.__len__() != 0:
+                update_sql += ', fi_unit_status=%(state)s'
+            else:
+                update_sql += 'fi_unit_status=%(state)s'
+            update_data['state'] = web_data['state']
+
+        if update_data.__len__() > 0:
+            update_data['id'] = data_base_data['fi_unit_id']
+            update_data_list.append(update_data)
+    return update_sql, update_data_list, update_data
 
 
 def run_task():
@@ -165,9 +164,10 @@ def test1():
 def test2():
     print('lllllllll')
 
+
 # set_task(test, when_time='seconds', every=10)
 # set_task(test1, when_time='seconds', every=35)
 # set_task(test2, when_time='seconds', every=50)
 # set_task(get_org_task, when_time=configs['task']['when_time'], at_day_start_time=configs['task']['at_day_start_time'])
-# set_task(get_org_task, when_time='seconds', every=15)
-# run_task()
+set_task(get_org_task, when_time='seconds', every=15)
+run_task()
